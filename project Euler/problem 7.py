d529dd49b806831b13430df1003d8adbb7880b57
DpsEagle <91342396+DpsEagle@users.noreply.github.com>
2021-12-03 19:04:20 +0000

    Update problem 7.py

--- project Euler/problem 7.py
+++ project Euler/problem 7.py
@@ -1,9 +1,9 @@
 
 
-def isPrime(n):
+def isPrime(num):
 
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    for x in range(2, int(num**0.5) + 1):
+        if x % i == 0:
             return False
     return True
 
